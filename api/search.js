302a963af534595d3de7dcedfc8331246165f803
Adam Ridovics <adam.ridovics@gmail.com>
2023-10-03 00:04:12 +0200

    fix(index, search): remove logs & comments

--- api/search.js
+++ api/search.js
@@ -6,7 +6,6 @@ module.exports = async (req, res) => {
   if (!query) return res.status(400).send("A search query is required.");
 
   try {
-    // 1. Metaphor Client
     const metaphorClient = axios.create({
       baseURL: "https://api.metaphor.systems",
       headers: {
@@ -16,7 +15,6 @@ module.exports = async (req, res) => {
 
     const metaphorResults = await metaphorClient.post("/search", { query });
 
-    // 2. SerpAPI Client
     const serpApiClient = axios.create({
       baseURL: "https://serpapi.com",
     });
@@ -24,13 +22,11 @@ module.exports = async (req, res) => {
     const serpApiResults = await serpApiClient.get("/search", {
       params: {
         q: query,
-        // q: "Coffee",
         engine: "google",
         api_key: process.env.SERP_API_KEY,
       },
     });
 
-    // 3. Fetching Favicons for Metaphor Results
     const resultsWithFavicons = await Promise.all(
       metaphorResults.data.results.map(async (result) => {
         try {
@@ -44,25 +40,12 @@ module.exports = async (req, res) => {
       })
     );
 
-    // Setup CORS headers
-    // res.setHeader("Access-Control-Allow-Origin", "*"); // allow any origin to access
-    // res.setHeader("Access-Control-Allow-Methods", "GET"); // only allow GET
-
-    // console.log("serpApiResults.data.organic_results: ", serpApiResults.data.organic_results);
-    // console.log("resultsWithFavicons: ", resultsWithFavicons);
-
-    // 4. Structure the response to include results from both APIs
     res.status(200).json({
       metaphorResults: resultsWithFavicons,
       serpApiResults: serpApiResults.data.organic_results,
     });
   } catch (error) {
     console.error(error);
-
-    // Setup CORS headers in case of an error too
-    // res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader("Access-Control-Allow-Methods", "GET");
-
     res.status(500).send("Internal Server Error");
   }
 };
